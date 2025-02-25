1626dc9ae923f0db0f4e7056d684fc0b5d8eac24
Franklin <writefranklyn@gmail.com>
2025-02-25 23:31:58 +0100

    add view to return predicted price in json format

--- ml_api/predictor/views.py
+++ ml_api/predictor/views.py
@@ -1,2 +1,18 @@
+# predictor/views.py
 import joblib
-import numpy as np
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from predictor import PredictionSerializer
+
+model = joblib.load("predictor/ml_model.pkl")
+
+@api_view(['POST'])
+def predict_price(request):
+    serializer = PredictionSerializer(data=request.data)
+    if serializer.is_valid():
+        bedrooms = serializer.validated_data['Bedrooms']
+        toilets = serializer.validated_data['Toilets']
+        parkingspace = serializer.validated_data['Parking_Space']
+        prediction = model.predict([[bedrooms, toilets, parkingspace]])[0]
+        return JsonResponse({"Predicted Price": f"₦{round(prediction, 2)}"})
+    return JsonResponse(serializer.errors, status=400)
